53396ab5427471cfc8b5a3647a4b35cb8f5a011a
Paul Brady <pablo.brady@gmail.com>
2020-05-11 20:05:37 -0700

    wip

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const express = require('express');
 let app = express();
 
+console.log("RUNNING...")
 // const users = require('./fixtures/users.json');
- 
+
 app.use((req, res) => {
 	let route = req.method + ' ' + req.url;
 
+  console.log(req.accepts('json'))
+  console.log(req.accepts('xml'))
+
+  /* Issue! */
   if (req.accepts(['json', 'text'])) {
+    res.type('json');
     res.sendFile("/", {
        root: "./fixture/users.json"
     });
-    res.status(200).end();
+    res.status(200);
   } else if (req.accepts('cvs')) {
+    res.type('cvs');
     res.sendFile("/", {
        root: "./fixture/users.cvs"
     });
-    res.status(200).end();
+    res.status(200);
   } else if (req.accepts('xml')) {
+    res.type('xml');
     res.sendFile("/", {
        root: "./fixture/users.xml"
     });
-    res.status(200).end();
+    res.status(200);
   } else {
     res.render('406', 'Content-type not supported');
   }
+
+  res.end()
 
 });
  
